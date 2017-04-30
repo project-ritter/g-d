353a8f981449fd312dba0433f62338f435788758
bmeritter <bme_ritter@foxmail.com>
2017-04-30 11:32:18 +0800

    add paper header

--- client/src/components/paper/index.js
+++ client/src/components/paper/index.js
@@ -1,14 +1,21 @@
 import React, {Component} from 'react';
 import rd3 from 'react-d3';
 
+import PaperHeader from './paper-header';
+
 let PieChart = rd3.PieChart;
 
 export default class Paper extends Component {
   render() {
     var pieData = [{label: "Margarita", value: 20.0}, {label: "John", value: 55.0}, {label: "Tim", value: 25.0}];
-    return (<div>
-      <PieChart data={pieData} width={450} height={400} radius={110} innerRadius={20} sectorBorderColor="white"
-                title="Pie Chart"/>
-    </div>);
+    return (
+      <div className="stack">
+
+        <PaperHeader/>
+
+        <PieChart data={pieData} width={450} height={400} radius={110} innerRadius={20} sectorBorderColor="white"
+                  title="Pie Chart"/>
+      </div>
+    );
   }
 }
---
+++ client/src/components/paper/paper-header.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class ScoreHeader extends Component {
+  render() {
+    return (
+      <div className='stack-header'>
+        试卷分析
+      </div>
+    );
+  }
+}

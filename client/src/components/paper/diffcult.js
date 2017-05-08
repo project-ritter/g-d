3597780b5f018d4b3b1531d749eaaae02c5ad31e
bmeritter <bme_ritter@foxmail.com>
2017-05-08 17:08:28 +0800

    difficult table

--- client/src/components/paper/diffcult.js
+++ client/src/components/paper/diffcult.js
@@ -9,6 +9,7 @@ export default class Difficult extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      difficult: [],
       barData: [{
         name: '',
         values: []
@@ -23,12 +24,12 @@ export default class Difficult extends Component {
         if (err) {
           throw err;
         }
-        console.log(res.body);
         this.setState({
           barData: [{
             name: '',
             values: res.body
-          }]
+          }],
+          difficult: res.body
         });
       })
   }
@@ -36,10 +37,46 @@ export default class Difficult extends Component {
   render() {
     return (
       <div>
-        <BarChart data={this.state.barData} width={500} height={300}
-                  title="难度分析结果" yAxisLabel="难度越小题目越难"
-                  xAxisLabel=''/>
+        <div className="col-sm-6">
+          <BarChart data={this.state.barData} width={500} height={300}
+                    title="难度分析结果" yAxisLabel="难度越小题目越难"
+                    xAxisLabel=''/>
+
+        </div>
+        <div className="col-sm-6 difficult">
+          <table className="table table-striped table-bordered table-hover">
+            <thead>
+            <tr>
+              <th>题目</th>
+              {
+                this.state.difficult.map((item, index) => {
+                  return (
+                    <th key={index}>
+                      {item.x}
+                    </th>
+                  );
+                })
+              }
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+              <td>难度</td>
+              {
+                this.state.difficult.map((item, index) => {
+                  return (
+                    <td>{item.y}</td>
+                  );
+                })
+              }
+            </tr>
+            </tbody>
+
+          </table>
+        </div>
+
       </div>
+
     );
   }
 }

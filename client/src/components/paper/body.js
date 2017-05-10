ca6612f442acd2059e0530dedc50c18b30f43a6e
bmeritter <bme_ritter@foxmail.com>
2017-05-10 19:49:33 +0800

    currentProgram

--- client/src/components/paper/body.js
+++ client/src/components/paper/body.js
@@ -1,16 +1,26 @@
 import {Component} from 'react';
 
+import ProgramType from './program-type';
 import Difficult from './difficult';
 import Distinct from './distinct';
 import Validate from './validate';
 
 export default class Body extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentProgram: 0
+    }
+
+  }
+
   render() {
     return (
       <div>
-        <Difficult/>
-        <Distinct/>
-        <Validate/>
+        <ProgramType currentProgram={this.state.currentProgram}/>
+        <Difficult currentProgram={this.state.currentProgram}/>
+        <Distinct currentProgram={this.state.currentProgram}/>
+        <Validate currentProgram={this.state.currentProgram}/>
       </div>
     );
   }

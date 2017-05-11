11f548cad6aaf8a921615d4140494ffced3644e7
bmeritter <bme_ritter@foxmail.com>
2017-05-11 19:35:10 +0800

    email input & button

--- client/src/components/score/score-header.js
+++ client/src/components/score/score-header.js
@@ -3,8 +3,21 @@ import React, {Component} from 'react';
 export default class ScoreHeader extends Component {
   render() {
     return (
-      <div className='stack-header'>
-        成绩分析
+      <div>
+        <div className='stack-header'>
+          成绩分析
+        </div>
+        <div className="form-horizontal email">
+          <div className="form-group">
+
+            <div className="col-sm-5">
+              <input type="email" className="form-control" id="inputEmail3" placeholder="Email"/>
+
+            </div>
+            <button type="submit" className="btn btn-info">获取成绩分析结果</button>
+
+          </div>
+        </div>
       </div>
     );
   }
